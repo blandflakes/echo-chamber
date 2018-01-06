97e165b6a6499300540ced80d1d9aaec1018029c
blandflakes <xclite@gmail.com>
2018-01-05 20:28:11 -0500

    Bump clojure version.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject echo-chamber "0.3.1"
+(defproject echo-chamber "0.3.2"
   :url "https://github.com/blandflakes/echo-chamber"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :description "SDK for building echo apps in Clojure."
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.taoensso/timbre "4.10.0"]])
